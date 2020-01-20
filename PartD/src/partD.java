a64014a845868e9fef8851dfe87a860b35c07035
ecunningham9000 <ecunningham9000@gmail.com>
2020-01-20 23:46:22 +1300

    Commit test 3

--- PartD/src/partD.java
+++ PartD/src/partD.java
@@ -2,7 +2,7 @@
 public class partD {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 		System.out.println("Hi, my Name is Ethan");
 		System.out.println("\nMy favourite colour is Blue");
