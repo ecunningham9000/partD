9c764b65809b5eb7ac9fcd9f14c56fdfc07c199d
ecunningham9000 <ecunningham9000@gmail.com>
2020-01-20 23:48:48 +1300

    Top comment

--- PartD/src/partD.java
+++ PartD/src/partD.java
@@ -3,7 +3,8 @@ public class partD {
 
 	public static void main(String[] args) {
 		
-
+		// BIT504 Assignment 2 Part D 
+		
 		System.out.println("Hi, my Name is Ethan");
 		System.out.println("\nMy favourite colour is Blue");
 		System.out.println("\nMy avourite mode of transport is my car");
